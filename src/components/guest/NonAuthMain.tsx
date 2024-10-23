73876b56bf3cca51f2d43f069cac217a2a75a6f1
loveytheb <cjstjsn99@gamil.com>
2024-10-23 21:28:42 +0900

    [#8] feat: 경로 설정 - 로그인, 회원가입 버튼 경로 - 비로그인 회원만 접근 가능

--- src/components/guest/NonAuthMain.tsx
+++ src/components/guest/NonAuthMain.tsx
@@ -1,4 +1,16 @@
+"use client";
+
+import { useAuthStore } from "@/src/store/auth/authStore";
+import { useRouter } from "next/navigation";
+
 const NonAuthMain = () => {
+  const router = useRouter();
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+
+  if (isLoggedIn) {
+    return null;
+  }
+
   return (
     <div className="flex h-screen items-center justify-center bg-gray-50">
       <div className="flex w-full max-w-7xl justify-between">
@@ -16,7 +28,7 @@ const NonAuthMain = () => {
         <div className="w-1/2 flex flex-col items-center justify-center space-y-4">
           <h2 className="text-3xl font-semibold mb-6">지금 가입하세요</h2>
 
-          <button className="bg-blue-600 hover:bg-blue-00 text-white px-6 py-3 w-72 rounded-full shadow-lg text-lg transition">
+          <button className="bg-blue-600 hover:bg-blue-500 text-white px-6 py-3 w-72 rounded-full shadow-lg text-lg transition">
             Google 계정으로 가입하기
           </button>
           <button className="bg-black hover:bg-gray-800 text-white px-6 py-3 w-72 rounded-full shadow-lg text-lg transition">
@@ -25,11 +37,17 @@ const NonAuthMain = () => {
           <div className="text-gray-400">
             --------------------- 또는 ---------------------
           </div>
-          <button className="bg-black hover:bg-gray-800 text-white px-6 py-3 w-72 rounded-full shadow-lg text-lg transition">
+          <button
+            className="bg-black hover:bg-gray-800 text-white px-6 py-3 w-72 rounded-full shadow-lg text-lg transition"
+            onClick={() => router.push("/auth/register")}
+          >
             계정 만들기
           </button>
           <div className="mt-6 text-gray-500">이미 Tag에 가입하셨나요?</div>
-          <button className="bg-black hover:bg-gray-800 text-white px-6 py-3 w-72 rounded-full shadow-lg text-lg transition">
+          <button
+            className="bg-black hover:bg-gray-800 text-white px-6 py-3 w-72 rounded-full shadow-lg text-lg transition"
+            onClick={() => router.push("/auth/login")}
+          >
             로그인
           </button>
         </div>
